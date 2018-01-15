1d3a283d1f4d2a87b3f9c85ebe14d94ea64ad060
Courtney Osborn <courtney.osborn@ft.com>
2018-01-15 11:42:02 +0000

    next-ify
     🐿 v2.7.0

---
+++ Makefile
@@ -0,0 +1,11 @@
+# n-gage bootstrapping logic
+node_modules/@financial-times/n-gage/index.mk:
+	npm install --no-save --no-package-lock @financial-times/n-gage
+	touch $@
+
+-include node_modules/@financial-times/n-gage/index.mk
+
+export IGNORE_A11Y = true
+
+demo:
+	./node_modules/.bin/obt demo --run-server --watch
---
+++ secret-squirrel.js
@@ -0,0 +1,16 @@
+module.exports = {
+	files: {
+		allow: [
+			'demos/src/alert-banner-build.mustache',
+			'demos/src/alert-banner-error.mustache',
+			'demos/src/alert-banner-neutral.mustache',
+			'demos/src/alert-banner-success.mustache',
+			'demos/src/pa11y.mustache'
+		],
+		allowOverrides: []
+	},
+	strings: {
+		deny: [],
+		denyOverrides: []
+	}
+};
