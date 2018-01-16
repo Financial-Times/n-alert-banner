84cd3d38528795d3983756e318015e3dee9c865e
courtney <courtne.osborn@ft.com>
2018-01-16 17:45:21 +0000

    Removes o.DOMContentLoaded  🐿 v2.7.0

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
 import AlertBanner from './src/js/alert-banner';
 
-function constructAll () {
-	AlertBanner.init();
-	document.removeEventListener('o.DOMContentLoaded', constructAll);
-}
-
-document.addEventListener('o.DOMContentLoaded', constructAll);
-
 export default AlertBanner;
