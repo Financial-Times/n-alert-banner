724a08556749caab6f207396842b256670e4004f
courtney <courtne.osborn@ft.com>
2018-01-19 10:16:15 +0000

    Adds index to helpers  🐿 v2.7.0

---
+++ test/alert-tests/helpers/index.js
@@ -0,0 +1,9 @@
+import * as fixtures from './fixtures';
+import stubs from './stubs';
+import { createOneLineString } from './utils';
+
+export default {
+	fixtures,
+	stubs,
+	createOneLineString
+};
