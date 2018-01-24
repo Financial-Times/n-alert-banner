4564a5cb70482b408017523d8822104eaf5dbc95
courtney <courtne.osborn@ft.com>
2018-01-24 16:57:52 +0000

    Removes callback and defaults to top of page  🐿 v2.7.0

--- src/js/lib/append-child.js
+++ src/js/lib/append-child.js
@@ -1,8 +1,11 @@
 export default function appendChild (parent, element) {
+
 	try {
 		parent.appendChild(element);
 	}
 	catch (err) {
 		console.warn(err);
+		document.body.insertBefore(element, document.body.firstChild);
 	}
+
 }
