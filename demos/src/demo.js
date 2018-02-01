d074756585dba87725f600665278ff1b9fb5632e
Adam Tavener <a.tavvy@gmail.com>
2018-02-01 16:01:36 +0000

    add a marketing (teal) theme  🐿 v2.7.0

--- demos/src/demo.js
+++ demos/src/demo.js
@@ -1,3 +1,25 @@
 import AlertBanner from '../../main.js';
 
-AlertBanner.init();
+const isClientInitialised = document.querySelector('#client-initialised-demo');
+
+if (isClientInitialised) {
+	const buttons = document.querySelectorAll('.buttons button');
+
+	buttons.forEach(el => {
+		const type = el.dataset['type'];
+		el.addEventListener('click', () => {
+			new AlertBanner(null, {
+				theme: type,
+				contentLong: `<p><b>Example ${type} message</b> Content Long block.</p>`,
+				contentShort: `<p>Content short block.</p>`,
+				buttonLabel: 'Button',
+				buttonUrl: '#try-button',
+				linkLabel: 'Text link',
+				linkUrl: '#feedback-link',
+				appendToElement: '#site-navigation'
+			});
+		});
+	});
+} else {
+	AlertBanner.init();
+}
